9671ef91764e82030f948ed4136389dcdb16dbdd
cihlar <martin.cihlar@ysoft.com>
2022-04-21 22:53:46 +0200

    refactoring

--- Transpose.cs
+++ Transpose.cs
@@ -1,24 +1,28 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 public static class Transpose
 {
-    private static string GetTransponsedLineForIndex(int lineIndex, IEnumerable<string> lines, List<int> linesSizes)
+    private static string GetTransposedLineFromCharsAtIndex(int charIndex, IEnumerable<string> lines,
+        IEnumerable<int> linesSizes)
     {
-        var line = string.Concat(lines.Select(line => lineIndex < line.Length ? line.Substring(lineIndex, 1) : " "));
-        var padding = linesSizes.Select((size, sizeIndex) => size > lineIndex ? sizeIndex + 1 : 0).Max();
-        return line.TrimEnd().PadRight(padding);
+        var charsAtIndex = lines.Select(line => charIndex < line.Length ? line[charIndex] : ' ');
+        var transponsedLine = string.Concat(charsAtIndex);
+        var padding = linesSizes
+            .Select((value, index) => (value, index))
+            .Last(size => size.value > charIndex)
+            .index + 1;
+
+        return transponsedLine.TrimEnd().PadRight(padding);
     }
 
     public static string String(string input)
     {
         var lines = input.Split('\n');
-        var linesSizes = lines.Select(line => line.Length).ToList();
-
+        var linesSizes = lines.Select(line => line.Length);
         var transposedLines = Enumerable.Range(0, linesSizes.Max())
-            .Select(index => GetTransponsedLineForIndex(index, lines, linesSizes));
+            .Select(index => GetTransposedLineFromCharsAtIndex(index, lines, linesSizes));
 
-        return string.Join('\n', transposedLines).TrimEnd();
+        return string.Join('\n', transposedLines);
     }
 }
